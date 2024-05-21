10adf42b149d6ded644db668f2950e25104e4b7e
Mikhail <113180339+vitoffer@users.noreply.github.com>
2024-05-21 16:54:54 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+document
+	.getElementById("mobileMenuShowButton")
+	.addEventListener("click", () => {
+		document.getElementById("mobileMenu").style.display = "block";
+	});
+document
+	.getElementById("mobileMenuCloseButton")
+	.addEventListener("click", () => {
+		document.getElementById("mobileMenu").style.display = "none";
+	});
